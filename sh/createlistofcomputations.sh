9d4d29d8f21f87249a85598255cf008e2873c75f
Diego <diego.conti@unimib.it>
2021-04-11 16:20:09 +0200

    corrected bash scripts

--- sh/createlistofcomputations.sh
+++ sh/createlistofcomputations.sh
@@ -2,5 +2,4 @@ cd `dirname $0`/..
 magma -b maxG:=20 outFile:=2-20.comp magma/createlistofcomputations.m
 magma -b minG:=21 maxG:=30 outFile:=21-30.comp magma/createlistofcomputations.m
 magma -b minG:=31 maxG:=40 outFile:=31-40.comp magma/createlistofcomputations.m
-magma -b minG:=41 maxG:=50 outFile:=41-50.comp magma/createlistofcomputations.m
 
--- sh/runcomputations.sh
+++ sh/runcomputations.sh
@@ -1,14 +1,6 @@
 cd `dirname $0`/..
 computations=100	#number of computations to be performed by each magma process
 nthreads=$2
-let memory=$3/$nthreads
 mkdir -p co
-hliðskjálf --computations $1 --valhalla bad.csv --script magma/runcomputation.m --workload $computations --schema schema.info --nthreads $nthreads --memory $memory --workoutput generators
-while [ $nthreads -gt 1 ] && [ -f bad.csv ] 
-#run again the computations that did not complete because memory ran out, increasing the memory limit and decreasing the number of threads
-do
-	rm bad.csv
-	let nthreads=nthreads/2
-	let memory=memory*2
-	hliðskjálf --computations $1 --valhalla bad.csv --script magma/runcomputation.m --workload $computations --schema schema.info --nthreads $nthreads --memory $memory --workoutput generators
-done
+hliðskjálf --computations $1 --valhalla bad.csv --script magma/runcomputation.m --workload $computations --schema schema.info --nthreads $nthreads --memory 128 --total-memory $3 --workoutput generators
+
