920b8d788ecc5c29f83bf51a214b6d6655edf8a3
Diego <diego.conti@unimib.it>
2021-04-16 20:05:23 +0200

    tweaked scripts

--- sh/createlistofcomputations.sh
+++ sh/createlistofcomputations.sh
@@ -1,5 +1,8 @@
 cd `dirname $0`/..
 magma -b maxG:=20 outFile:=2-20.comp magma/createlistofcomputations.m
 magma -b minG:=21 maxG:=30 outFile:=21-30.comp magma/createlistofcomputations.m
-magma -b minG:=31 maxG:=40 outFile:=31-40.comp magma/createlistofcomputations.m
+for g in {31..40}
+do
+	magma -b minG:=$g maxG:=$g outFile:=$g.comp magma/createlistofcomputations.m
+done
 
--- sh/runcomputations.sh
+++ sh/runcomputations.sh
@@ -1,5 +1,5 @@
 cd `dirname $0`/..
-computations=100	#number of computations to be performed by each magma process
+computations=10000	#number of computations to be performed by each magma process
 nthreads=$2
 hliðskjálf --computations $1 --valhalla bad.csv --script magma/runcomputation.m --workload $computations --schema schema.info --nthreads $nthreads --memory 128 --total-memory $3 --workoutput generators
 
